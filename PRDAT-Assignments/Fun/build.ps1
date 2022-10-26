061a590d30ecffd7485f8c05dddd89bb6a4cabc6
day0r <skoledanyal@gmail.com>
2022-10-26 17:00:52 +0200

    Assignment06 layout

--- PRDAT-Assignments/Fun/build.ps1
+++
@@ -1,5 +0,0 @@
-fsyacc --module FunPar FunPar.fsy
-fslex --unicode FunLex.fsl
-dotnet fsi -r ../fsharp/FsLexYacc.Runtime.dll Absyn.fs FunPar.fs FunLex.fs Parse.fs HigherFun.fs ParseAndRunHigher.fs
-
-#open ParseAndRunHigher;;
